db66343ddaeb436a75403d06244b02a3f540045b
cndesantana <charles.desantana@eawag.ch>
2016-01-09 02:22:15 +0100

    Adding outputs per-generation

--- test/plottimeseries.R
+++ test/plottimeseries.R
@@ -1,15 +1,19 @@
-dat<-read.table("RichnessPerGen_AnaG_10000.0_cost_0.001_MR_0.0101_VR_0.001.txt",sep=" ",header=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
+inputfile<-args[1];
+cost_<-unlist(strsplit(unlist(strsplit(inputfile,'M'))[1],'_'))[5];    
+
+dat<-read.table(inputfile,sep=" ",header=TRUE)
 ri<-1
 posrepl<-which(dat$Real==ri)
 nsites<-max(dat$Site)
 subdat<-dat[posrepl,]
 maxGen<-subdat$G[1]
 timeseries<-matrix(1,maxGen+1,nsites)
-for(i in 1:maxGen){for(j in 1:nsites){timeseries[i+1,j]<-subdat[j+nsites*(i-1),13];}}
+for(i in 1:maxGen){for(j in 1:nsites){timeseries[i+1,j]<-subdat$alpharich[j+nsites*(i-1)];}}
 
-png("./timeseries.png",width=1980,height=1280,res=300);
+png(paste("./timeseries_cost_",cost_,".png",sep=""),width=1980,height=1280,res=300);
 for(i in 1:nsites){
-if(i == 1){plot(timeseries[,i],xlab="Generations",ylab="Beta-Richness",main="Richness per site",type="b",col=i,ylim=c(0,150))}
+if(i == 1){plot(timeseries[,i],xlab="Generations",ylab="Beta-Richness",main=paste("Richness per site\ncost = ",cost_,sep=""),type="b",col=i,ylim=c(0,75))}
 else{points(timeseries[,i],col=i,type="b")}
 }
 legend("topleft",legend=c("lake1","lake2","lake3","lake4","lake5","lake6","lake7","lake8"),col=1:8,pch=1);
